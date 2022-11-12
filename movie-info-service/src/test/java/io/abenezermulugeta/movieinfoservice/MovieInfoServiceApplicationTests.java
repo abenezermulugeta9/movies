dca53e01b094bfc33da7a0dbc89052d6a33453ab
AbenezerM <abenezermulugeta41@gmail.com>
2022-11-12 02:15:59 -0600

    Initial commit - creating 3 projects

---
+++ movie-info-service/src/test/java/io/abenezermulugeta/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.abenezermulugeta.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
