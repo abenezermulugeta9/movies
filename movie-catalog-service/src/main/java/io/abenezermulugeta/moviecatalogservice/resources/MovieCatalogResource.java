39d33eb0fcc9e4f190f7bc5dc0b832c43fdd0827
AbenezerM <abenezermulugeta41@gmail.com>
2022-11-14 02:02:06 -0600

    Replace the review project with ratings project and use restTemplate to make a remote call between catalog and info microservices

--- movie-catalog-service/src/main/java/io/abenezermulugeta/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/abenezermulugeta/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,19 +1,31 @@
 package io.abenezermulugeta.moviecatalogservice.resources;
 
 import io.abenezermulugeta.moviecatalogservice.models.CatalogItem;
+import io.abenezermulugeta.moviecatalogservice.models.Movie;
+import io.abenezermulugeta.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
-
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        return Collections.singletonList(
-                new CatalogItem("Avengers", "Avengers Description", 5)
+        RestTemplate restTemplate = new RestTemplate();
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
         );
+
+        return ratings.stream()
+                .map(r -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + r.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "Test", r.getRating());
+                })
+                .collect(Collectors.toList());
     }
 }
